5d185aa97330ae61f45a78b1dc36d65130e9518a
Vladyslav Krutyus <kijdron@gmail.com>
2023-06-13 16:22:16 +0300

    initial commit

---
+++ src/main/java/Inspector.java
@@ -0,0 +1,12 @@
+import java.util.Map;
+
+public class Inspector {
+    public void receiveBulletin(String bulletin) {
+        // Your code here
+    }
+
+    public String inspect(Map<String, String> person) {
+        // Your code here
+        return "";
+    }
+}
